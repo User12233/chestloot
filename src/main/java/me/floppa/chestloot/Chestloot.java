d4b9aaf9281669d385896aa0279f81cb0d3bdaee
ffloppa <90894503+User12233@users.noreply.github.com>
2025-03-01 14:27:44 +0500

    fixes

--- src/main/java/me/floppa/chestloot/Chestloot.java
+++ src/main/java/me/floppa/chestloot/Chestloot.java
@@ -1,34 +1,47 @@
 package me.floppa.chestloot;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.logging.LogUtils;
+import me.floppa.chestloot.Modules.ChestLootConfig;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.server.ServerLifecycleHooks;
+import org.joml.Matrix4f;
 import org.slf4j.Logger;
+
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Chestloot.MODID)
@@ -50,12 +63,12 @@ public class Chestloot {
 
         REGISTER.register(modEventBus);
         ITEMS.register(modEventBus);
-        modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
+        MinecraftForge.EVENT_BUS.register(EventsHandler.class);
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER,ChestLootConfig.COMMON_CONFIG);
     }
 
     // Add the example block item to the building blocks tab
@@ -63,18 +76,92 @@ public class Chestloot {
         if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) event.accept(CHESTCOPY_ITEM);
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {
-        if(ModList.get().getMods().stream().noneMatch(mod -> mod.getModId().equals("battlecore"))) {
-            for (int i = 0; i <= 50; i++) {
-                LOGGER.error("GLEB EBLAN, INSTALL https://github.com/User12233/BattleCore");
-            }
-        }
-    }
-
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
         LOGGER.info("chestLoot is fine");
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD,modid = MODID,value = Dist.DEDICATED_SERVER)
+    public static class EventsHandler {
+        @SubscribeEvent
+        public static void onPlayerRMB(PlayerInteractEvent.RightClickBlock event) {
+            // Получаем состояние блока по позиции клика
+            BlockState state = event.getEntity().level().getBlockState(event.getPos());
+            if (state.getBlock() != chestcopy.get() || state.getBlock() == Blocks.AIR) {
+                LogUtils.getLogger().warn("Seems something isn't correct ");
+                return;
+            }
+            LOGGER.info("chestLoot executed");
+            MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+            server.getGameRules().getRule(GameRules.RULE_SENDCOMMANDFEEDBACK).set(false,server);
+
+            Objects.requireNonNull(server.getLevel(Level.OVERWORLD)).setBlock(event.getPos(),Blocks.AIR.defaultBlockState(),3);
+            Random rand = new Random();
+            for(int i = 0; i<=rand.nextInt(1,5);i++) {
+                try {
+                    server.getCommands().getDispatcher().execute(server.getCommands().getDispatcher().parse("give " + event.getEntity().getName().getString() + " " + getRandomItem(), server.createCommandSourceStack()));
+                } catch(CommandSyntaxException e) {
+                    LogUtils.getLogger().error("Failed to execute command ", e);
+                }
+            }
+            Executors.newSingleThreadScheduledExecutor().schedule(() -> {
+                // Этот код выполняется в отдельном потоке, поэтому переключаемся на главный серверный поток:
+                ServerLifecycleHooks.getCurrentServer().execute(() -> {
+                    Objects.requireNonNull(ServerLifecycleHooks.getCurrentServer().getLevel(Level.OVERWORLD)).setBlock(event.getPos(),Blocks.CHEST.defaultBlockState(),3);
+                });
+            }, 75L, TimeUnit.MINUTES);
+        }
+
+        private static String getRandomItem() {
+            Random rand = new Random();
+            if(rand.nextInt() < 0.2) {
+                if(rand.nextBoolean()) {
+                    return ChestLootConfig.LootTable.get().get(rand.nextInt(31, 38));
+                } else {
+                    String result = ChestLootConfig.LootTable.get().get(rand.nextInt(0, 30));
+                    if (result.contains("AmmoId") || result.contains("cooked_beef")) {
+                        return result;
+                    } else {
+                        return result + " 1";
+                    }
+                }
+            } else {
+                return ChestLootConfig.LootTable.get().get(0);
+            }
+        }
+        @SubscribeEvent
+        public static void onPlayerCommandPreprocess(ServerChatEvent event) {
+            LOGGER.info(event.getRawText());
+            LOGGER.info(event.getMessage().getString());
+            LOGGER.info(event.getUsername());
+
+        }
+        @SubscribeEvent
+        @OnlyIn(Dist.CLIENT)
+        public static void onRenderGui(RenderGuiEvent.Post event) {
+            Minecraft mc = Minecraft.getInstance();
+            if (mc.screen instanceof TitleScreen) {
+                PoseStack poseStack = event.getGuiGraphics().pose();
+
+                String text = "[CL] Fine, check desc";
+
+                int screenWidth = event.getWindow().getScreenWidth();
+                int screenHeight = event.getWindow().getScreenHeight();
+                int textWidth = mc.font.width(text);
+                float x = screenWidth - textWidth - 10;
+                float y = screenHeight - mc.font.lineHeight - 10;
+
+                int color = 0xFFFFFF;
+                Matrix4f matrix = poseStack.last().pose();
+                MultiBufferSource buffer = mc.renderBuffers().bufferSource();
+                Font.DisplayMode displayMode = Font.DisplayMode.NORMAL;
+                int packedLight = 15728880;
+                int packedOverlay = 0;
+
+                int renderedWidth = mc.font.drawInBatch(text,x,y,color,true,matrix,buffer,displayMode,packedLight,packedOverlay);
+            }
+        }
     }
 }
---
+++ src/main/java/me/floppa/chestloot/Modules/ChestLootConfig.java
@@ -0,0 +1,65 @@
+package me.floppa.chestloot.Modules;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ChestLootConfig {
+    // The common configuration spec
+    public static final ForgeConfigSpec COMMON_CONFIG;
+
+    // Example configuration option
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> LootTable;
+
+    static {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+
+        builder.push("General Settings");
+        // Define a boolean config option; default value is true
+        LootTable = builder
+                .comment("Define items for drop from chests")
+                .define("LootTable", Arrays.asList("minecraft:air",
+                        "securitycraft:keypad_chest",
+                        "securitycraft:camera_monitor",
+                        "securitycraft:remote_access_mine",
+                        "securitycraft:stone_mine",
+                        "securitycraft:mine",
+                        "peterswarfare:flashbang",
+                        "peterswarfare:fraggrenade",
+                        "peterswarfare:smokegrenade",
+                        "tacz:modern_kinetic_gun{\"GunId\":\"tacz:m16a4\",\"GunCurrentAmmoCount\": 31,\"HasBulletInBarrel\": 1b,\"GunFireMode\": \"SEMI\"}",
+                        "tacz:modern_kinetic_gun{\"GunId\":\"tacz:m320\",\"GunCurrentAmmoCount\": 1,\"HasBulletInBarrel\": 1b,\"GunFireMode\": \"SEMI\"}",
+                        "tacz:modern_kinetic_gun{\"GunId\":\"tacz:ak47\",\"GunCurrentAmmoCount\": 31,\"HasBulletInBarrel\": 1b,\"GunFireMode\": \"AUTO\"}",
+                        "tacz:modern_kinetic_gun{\"GunId\":\"tacz:m4a1\",\"GunCurrentAmmoCount\": 30,\"HasBulletInBarrel\": 1b,\"GunFireMode\": \"AUTO\"}",
+                        "tacz:modern_kinetic_gun{\"GunId\":\"tacz:aug\",\"GunCurrentAmmoCount\": 30\"HasBulletInBarrel\": 1b,\"GunFireMode\": \"AUTO\"}",
+                        "tacz:modern_kinetic_gun{\"GunId\":\"tacz:ai_awp\",\"HasBulletInBarrel\": 1b,\"\"GunFireMode\": \"SEMI\",\"GunCurrentAmmoCount\": 5}",
+                        "tacz:modern_kinetic_gun{\"GunId\":\"tacz:m700\",\"GunCurrentAmmoCount\": 6,\"HasBulletInBarrel\": 1b,\"GunFireMode\": \"SEMI\"}",
+                        "tacz:modern_kinetic_gun{\"GunId\":\"tacz:deagle\",\"GunCurrentAmmoCount\": 8,\"HasBulletInBarrel\": 1b,\"GunFireMode\": \"SEMI\"}",
+                        "tacz:attachment{\"AttachmentId\":\"tacz:muzzle_silencer_ptilopsis\"}",
+                        "tacz:attachment{\"AttachmentId\":\"tacz:muzzle_silencer_knight_qd\"}",
+                        "tacz:attachment{\"AttachmentId\":\"gucci_attachments:scope_karrina\"}",
+                        "tacz:attachment{\"AttachmentId\":\"tacz:sight_uh1\"}",
+                        "tacz:attachment{\"AttachmentId\":\"gucci_attachments:grip_vert2\"}",
+                        "tacz:ammo{\"AmmoId\":\"tacz:12g\"} 36",
+                        "tacz:ammo{\"AmmoId\":\"tacz:338\"} 30",
+                        "tacz:ammo{\"AmmoId\":\"tacz:40mm\"} 6",
+                        "tacz:ammo{\"AmmoId\":\"tacz:556x45\"} 60",
+                        "tacz:ammo{\"AmmoId\":\"tacz:9mm\"} 60",
+                        "tacz:ammo{\"AmmoId\":\"tacz:762x39\"} 60",
+                        "tacz:ammo{\"AmmoId\":\"tacz:308\"} 48",
+                        "minecraft:cooked_beef 9",
+                        "minecraft:golden_apple",
+                        "minecraft:netherite_helmet",
+                        "minecraft:netherite_chestplate",
+                        "minecraft:netherite_leggins",
+                        "minecraft:netherite_boots",
+                        "minecraft:diamond_helmet",
+                        "minecraft:diamond_chestplate",
+                        "minecraft:diamond_leggins",
+                        "minecraft:diamond_boots"),o -> o instanceof String);
+        builder.pop();
+
+        COMMON_CONFIG = builder.build();
+    }
+}
